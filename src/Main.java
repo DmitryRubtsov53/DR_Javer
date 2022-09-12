dc9c7bc61959f79b6e153701867a53b1df417175
DmitryRubtsov53 <2578390@mail.ru>
2022-09-12 19:22:14 +0500

    Поменяли текст для вывода на экран

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Мир!");
+        System.out.println("Домашнее задание Циклы - Повышенная сложность");
+
+        System.out.println(" ");
+        System.out.println("Задание 4 - Ping pong");
+
+                for (int i = 1; i <= 30; i++) {
+                    if (i % 3 == 0 && i % 5 == 0) { System.out.println(i + " ping pong"); }
+                    else if (i % 5 == 0) { System.out.println(i + " pong"); }
+                    else if (i % 3 == 0) { System.out.println(i + " ping"); }
+                    else System.out.println(i);
+            }
+
+        System.out.println(" ");
+        System.out.println("Задание 5 - Последовательность Фибоначи из 64 чисел");
+                long x = 0; // первое число ряда
+                long y = 1; // второе число ряда
+
+        System.out.print(x + " ");
+        System.out.print(y + " ");
+        int i = 1;
+        while ( i < 63 ) {
+            long tmp = y;
+            y = x + y;
+            x = tmp;
+            i++;
+            System.out.print(y + " ");
+        }
     }
 }
