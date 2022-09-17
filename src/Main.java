d946a43b90986f088ce1043e6ccb4f2c3732bb7b
DmitryRubtsov53 <2578390@mail.ru>
2022-09-17 10:16:20 +0500

    + Использовал Новый для меня способ решения с вызовом внешнего для Main метода: Задача 2 - Найти минимальную и максимальную трату в день.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Arrays; // добавил для Задачи 2.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашние задания * Повышенная сложность");
